6c58be5fa1e08605703215eb72b6136dc58bbe56
Ben Elago <benelago25@gmail.com>
2020-04-02 18:09:16 -0500

    Added Customer to Active Admin Resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :details
   resources :orders
   resources :customers
