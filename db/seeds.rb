aa8f56b776ba59c0b114ab98c9a999cd381ca7f5
Ben Elago <benelago25@gmail.com>
2020-04-03 12:33:21 -0500

    Completed seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,85 +1,43 @@
-require 'nokogiri'
-require 'httparty'
-require 'byebug'
-
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-# def tax_scraper
-#     url = 'http://www.calculconversion.com/sales-tax-calculator-hst-gst.html'
-#     unparsed_page = HTTParty.get(url)
-#     parsed_page = Nokogiri::HTML(unparsed_page)
-#     tax_list = parsed_page.css('tr')
-  
-#     taxes = []
-  
-#     tax_list.each.with_index do |tax, index|
-#       if index != 0
-#         tax_by_province = {
-#           province: tax.css('th').text,
-#           gst: tax.css('td')[1].text.strip.sub('%', ''),
-#           pst: tax.css('td')[2].text.strip.sub('%', '')
-#         }
-#         taxes << tax_by_province
-#       end
-#     end
-#     taxes
-#   end
-  
-#   TAXES = tax_scraper
-  
-#   TAXES.each do |tax|
-#     Province.create(name: province["name"])
-#   end
+scrap = Scraper.new
 
-
-
-
-def gungeon
-    url = 'https://gungeongod.com/'
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    guns = parsed_page.css('section:first-of-type').css('.sp-text')
-    
-    list = []
-    
-    guns.each do |gun|
-      x = {
-        name: gun.css('h3').text,
-        description: gun.css('ul:first-of-type').css('li:first-of-type').text,
-        guntype: gun.css('ul:nth-of-type(2n)').css('li:first-of-type').text
-        # quality: gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class'] == nil ? "" : gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class']
-        }
-        list << x
-    end
-    list
-end
-
-guns = gungeon
-
-randomprice = rand(10..999)
-
-guns.each 
-
-
-
-
-
-
+taxes = scrap.scrape_taxes
+guns = scrap.gungeon
 
 shoottype = ["Automatic","Beam","Burst","Charged","Semiautomatic","Varies"]
+qualitygrades = ('A'..'F').to_a
+
+# Seeding for provinces and taxes
+taxes.each do |tax|
+  province = tax[:province]
+  province_obj = Province.create(name: province)
+  taxes = tax[:tax_names]
+  pst = tax[:prov_tax]
+  gst = tax[:govt_tax]
+  taxes.each do |tax|
+    if (tax.strip == 'GST')
+      province_obj.taxes.create(name: tax.strip, tax: gst)
+    else
+      province_obj.taxes.create(name: tax.strip, tax: pst)
+    end
+  end
+end
 
 shoottype.each do |shoot|
   Guntype.create(name: shoot)
 end
 
-
-qualitygrades = ('A'..'F').to_a
-
 qualitygrades.each do |grade|
   Quality.create(name: grade)
 end
 
+guns.each do |gun|
+  Product.create(guntype_id: Guntype.all.sample.id, quality_id: Quality.all.sample.id, name: gun[:name], description: gun[:description], price: rand(10..999))
+end
 
-
-puts "Created #{Quality.count}"
-puts "Created #{Guntype.count}"
+puts "Created #{Tax.count} Taxes."
+puts "Created #{Province.count} Provinces."
+puts "Created #{Quality.count} Qualities."
+puts "Created #{Guntype.count} Guntypes."
+puts "Created #{Product.count} Products."
