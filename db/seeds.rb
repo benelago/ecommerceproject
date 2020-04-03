3ec0ad7d2aa7ee4e8ce415d8f738107f1604a48e
Ben Elago <benelago25@gmail.com>
2020-04-03 00:36:39 -0500

    error fix on web scraping

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,65 @@
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-# def gungeon
-#     url = 'https://gungeongod.com/'
+# def tax_scraper
+#     url = 'http://www.calculconversion.com/sales-tax-calculator-hst-gst.html'
 #     unparsed_page = HTTParty.get(url)
 #     parsed_page = Nokogiri::HTML(unparsed_page)
-#     guns = parsed_page.css('section:first-of-type').css('.sp-text')
-
-#     list = []
-
-#     guns.each do |gun|
-#         x = {
-#         name: gun.css('h3').text,
-#         description: gun.css('ul:first-of-type').css('li:first-of-type').text,
-#         guntype: gun.css('ul:nth-of-type(2n)').css('li:first-of-type').text
-#         # quality: gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class'] == nil ? "" : gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class']
+#     tax_list = parsed_page.css('tr')
+  
+#     taxes = []
+  
+#     tax_list.each.with_index do |tax, index|
+#       if index != 0
+#         tax_by_province = {
+#           province: tax.css('th').text,
+#           gst: tax.css('td')[1].text.strip.sub('%', ''),
+#           pst: tax.css('td')[2].text.strip.sub('%', '')
 #         }
-#         list << x
+#         taxes << tax_by_province
+#       end
 #     end
-#     guns
-# end
+#     taxes
+#   end
+  
+#   TAXES = tax_scraper
+  
+#   TAXES.each do |tax|
+#     Province.create(name: province["name"])
+#   end
 
-# pp gungeon
+
+
+
+def gungeon
+    url = 'https://gungeongod.com/'
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    guns = parsed_page.css('section:first-of-type').css('.sp-text')
+  
+    list = []
+  
+    guns.each do |gun|
+      x = {
+        name: gun.css('h3').text,
+        description: gun.css('ul:first-of-type').css('li:first-of-type').text,
+        guntype: gun.css('ul:nth-of-type(2n)').css('li:first-of-type').text
+        # quality: gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class'] == nil ? "" : gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class']
+        }
+        list << x
+    end
+    list
+  end
+  
+  test = gungeon
+
+pp test
+
+test = ('A'..'F').to_a.sample
+
+puts "#{test}"
+
+puts "Created #{Province.count} provinces"
