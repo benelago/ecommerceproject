dddb675dc4b4a5d5cc43a5abc21595afd6c9aeee
Ben Elago <benelago25@gmail.com>
2020-04-02 22:39:10 -0500

    Added contact and about page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_230713) do
+ActiveRecord::Schema.define(version: 2020_04_03_032135) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -93,6 +93,14 @@ ActiveRecord::Schema.define(version: 2020_04_02_230713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,27 @@ require 'byebug'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
 
-def gungeon
-    url = 'https://gungeongod.com/'
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    guns = parsed_page.css('section:first-of-type').css('.sp-text')
+# def gungeon
+#     url = 'https://gungeongod.com/'
+#     unparsed_page = HTTParty.get(url)
+#     parsed_page = Nokogiri::HTML(unparsed_page)
+#     guns = parsed_page.css('section:first-of-type').css('.sp-text')
 
-    list = []
+#     list = []
 
-    guns.each do |gun|
-        x = {
-        name: gun.css('h3').text,
-        description: gun.css('ul:first-of-type').css('li:first-of-type').text,
-        guntype: gun.css('ul:nth-of-type(2n)').css('li:first-of-type').text
-        # quality: gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class'] == nil ? "" : gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class']
-        }
-        list << x
-    end
-    guns
-end
+#     guns.each do |gun|
+#         x = {
+#         name: gun.css('h3').text,
+#         description: gun.css('ul:first-of-type').css('li:first-of-type').text,
+#         guntype: gun.css('ul:nth-of-type(2n)').css('li:first-of-type').text
+#         # quality: gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class'] == nil ? "" : gun.css('ul:nth-of-type(2n)').css('li:nth-of-type(4n)').css('span')['class']
+#         }
+#         list << x
+#     end
+#     guns
+# end
 
-pp gungeon
+# pp gungeon
